911c4fec689d2f581b0e7dd6038e95048f81a098
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2023-02-07 13:57:34 +0000

    Tamanho do mosquito

--- jogo.js
+++ jogo.js
@@ -11,7 +11,7 @@ function ajustaTamanhoPalcoJogo() {
 ajustaTamanhoPalcoJogo()
 
 
-//Posição do mosquito
+//Posição da mosquito
 
 function posicaoRandomica() {
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -24,13 +24,27 @@ function posicaoRandomica() {
     //criar o elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosquito.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
    
     document.body.appendChild(mosquito)
+
+}
+
+// tamanho radômico do mosquito
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
     
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
 }
 
 
