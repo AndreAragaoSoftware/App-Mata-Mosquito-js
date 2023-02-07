61a855ce9145eefb26c07cb00d8581ca4ed849b8
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2023-02-07 13:30:32 +0000

    crinado posições randômicas

--- jogo.js
+++ jogo.js
@@ -1,12 +1,36 @@
+//Ajuste de tela
 var altura = 0
 var largura = 0
 
 function ajustaTamanhoPalcoJogo() {
-    var altura = window.innerHeight
-    var largura = window.innerWidth
+    altura = window.innerHeight
+    largura = window.innerWidth
     console.log(altura, largura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+
+//Posição do mosquito
+
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    // posiçao recebe 0 se for menor que 0 caso contrario recebe ela mesma
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    //criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+   
+    document.body.appendChild(mosquito)
+    
+}
 
 
